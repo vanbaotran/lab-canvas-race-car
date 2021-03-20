2f8f2b265fbdd2c8b639d8747b59fccfed639a39
Tran Van <vanbaotran201194@gmail.com>
2021-03-20 15:50:16 +0100

    iter 1 2

--- js/car.js
+++ js/car.js
@@ -1,26 +1,36 @@
 class Car {
   constructor() {
+   
     const img = document.createElement('img');
     img.onload = () => {
       this.img = img;
-
       const imgRatio = img.naturalWidth/img.naturalHeight;
-
+      console.log('chargee',img.naturalWidth,img.naturalHeight)
       // TODO
+      this.w = 100;
+      this.h = this.w/imgRatio;
+      this.x = W/2-this.w/2;
+      this.y = H-this.h;
     }
     img.src = "images/car.png";
   }
 
   draw() {
     if (!this.img) return; // if `this.img` is not loaded yet => don't draw
+    ctx.clearRect(0,0,W,H);
+    ctx.drawImage(this.img,this.x,this.y,this.w,this.h)
 
     // TODO
   }
 
   moveLeft() {
     // TODO
+    this.x--;
+    this.draw();
   }
   moveRight() {
     // TODO
+    this.x++;
+    this.draw();
   }
 }
--- js/main.js
+++ js/main.js
@@ -10,16 +10,27 @@ const H = ctx.canvas.height;
 function draw() {
   //
   // Iteration 1: road drawing
-  //
-
+  
+  ctx.fillStyle = 'green';
+  ctx.fillRect(0, 0, 30, canvas.height);
+  ctx.fillRect(canvas.width - 30, 0, 30, canvas.height);
+  ctx.fillStyle = 'dimgray';
+  ctx.fillRect(30, 0, 10, canvas.height);
+  ctx.fillRect(canvas.width - 40, 0, 10, canvas.height);
+  ctx.fillRect(50, 0, canvas.width - 100, canvas.height);
+  let y=0;
+  for (let i=0;i<45;i++){
+    ctx.fillStyle='white';
+    y=i*50;
+    ctx.fillRect(W/2+5,y,5,20);
+  }
   // TODO
 
   //
   // Iteration 2: car drawing
-  //
-
+  //  
   // TODO
-
+  car.draw();
   //
   // Iteration #4: obstacles
   //
@@ -42,6 +53,17 @@ function draw() {
 
 document.onkeydown = function (e) {
   if (!car) return;
+  console.log('touche appuyee',e)
+  switch (e.key){
+    case 'ArrowLeft':
+      car.moveLeft();
+      car.draw();
+      break;
+    case 'ArrowRight':
+      car.moveRight();
+      car.draw();
+      break;
+  }
 
   // TODO
 }
@@ -62,7 +84,7 @@ function startGame() {
   if (raf) {
     cancelAnimationFrame(raf);
   }
-
+  car = new Car();
   // TODO
 
   animLoop();
